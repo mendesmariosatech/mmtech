93ea732a5df581ba85587fdb087849c86217f647
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-08-29 14:45:38 -0400

    chore: set share code on new project

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@repo/ui/globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+import { AuthenticationPage } from "@repo/ui/components/molecules/SignIn/AuthenticationPage";
 
+export default function Page() {
+  return (
+    <main>
+      <AuthenticationPage />
     </main>
   );
 }
