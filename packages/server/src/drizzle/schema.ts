3cbc4d3de6d192d7015c42917333b8abb0fd3076
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-09-08 18:50:05 -0400

    feat: drizzle config

---
+++ packages/server/src/drizzle/schema.ts
@@ -0,0 +1,26 @@
+import { sql } from 'drizzle-orm';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const authTable = sqliteTable('auth', {
+  id: integer('id').primaryKey(),
+  passwordDigest: text('password_digest').notNull(),
+  email: text('email').unique().notNull(),
+});
+
+export const usersTable = sqliteTable('user', {
+  id: integer('id').primaryKey(),
+  name: text('name').notNull(),
+  authId: integer('auth_id')
+    .notNull()
+    .references(() => authTable.id, { onDelete: 'cascade' }),
+  createdAt: text('created_at')
+    .default(sql`(CURRENT_TIMESTAMP)`)
+    .notNull(),
+  updateAt: integer('updated_at', { mode: 'timestamp' }).$onUpdate(() => new Date()),
+});
+
+export type InsertAuth = typeof authTable.$inferInsert;
+export type SelectAuth = typeof authTable.$inferSelect;
+
+export type InsertUser = typeof usersTable.$inferInsert;
+export type SelectUser = typeof usersTable.$inferSelect;
