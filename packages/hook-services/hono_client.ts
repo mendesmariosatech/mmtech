7518196dad8de10ef4f1496e27c9a671e84581c8
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-09-30 00:31:33 -0400

    Update hono_client.ts

--- packages/hook-services/hono_client.ts
+++ packages/hook-services/hono_client.ts
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 3001;
 
 console.log({
   vercel: process.env.VERCEL_URL,
-  next: process.env.__NEXT_PRIVATE_ORIGIN,
+  next: process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL,
 })
 
-const URL = process.env.VERCEL_UR || process.env.__NEXT_PRIVATE_ORIGIN  // my-site.vercel.app || localhost:3001
+const URL = process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL  // my-site.vercel.app || localhost:3001
 
 
 const DEV_URL = `http://localhost:${PORT}/`;
