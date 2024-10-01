b301ab8beaa8262626d5cd2595ee835bb7095f33
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-09-30 22:04:33 -0400

    chore: fix erros

--- packages/hook-services/hono_client.ts
+++ packages/hook-services/hono_client.ts
@@ -9,13 +9,12 @@ console.log({
   next: process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL,
 })
 
-const URL = process.env.NEXT_PUBLIC_URL  // my-site.vercel.app || localhost:3001
-
+const URL = process.env.__NEXT_PRIVATE_ORIGIN  // my-site.vercel.app || localhost:3001
 
 const DEV_URL = `http://localhost:${PORT}/`;
 const PROD_URL = `https://mmtech-web.vercel.app/`;
 
 // const URL = PROD ? PROD_URL : DEV_URL;
-console.log("PROCESS", process.env);
+// console.log("PROCESS", process.env);
 
-export const hono_client = hc<AppType>(URL!);
+export const hono_client = hc<AppType>("http://localhost:3001/api");
