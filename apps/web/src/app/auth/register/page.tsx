92f1e369dd7b6e7f30cb00829ddcfec99b38bc86
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-10-05 15:47:09 -0400

    fix: web build error and move env example

--- apps/web/src/app/auth/register/page.tsx
+++ apps/web/src/app/auth/register/page.tsx
@@ -1,15 +1,4 @@
 "use client";
 import { RegisterPage } from "@repo/ui/components/domain/index";
 
-export default function Page() {
-	return (
-		<main>
-			<RegisterPage
-				isLoading={false}
-				handleLogIn={() => {
-					return Promise.resolve();
-				}}
-			/>
-		</main>
-	);
-}
+export default RegisterPage
