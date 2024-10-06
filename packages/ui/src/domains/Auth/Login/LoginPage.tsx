7a45bad988da628f071eb5e1e312109f2df443e6
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-10-06 13:42:08 -0400

    chore: Logo component and screen height login and register

--- packages/ui/src/domains/Auth/Login/LoginPage.tsx
+++ packages/ui/src/domains/Auth/Login/LoginPage.tsx
@@ -2,6 +2,8 @@
 import type { Metadata } from "next";
 import { LoginForm } from "@repo/ui/components/form-builder/Forms/Login/LoginForm";
 import Link from "next/link";
+import Image from "next/image";
+import { Logo } from "@repo/ui/components/ui/Logo";
 
 export const metadata: Metadata = {
 	title: "Authentication",
@@ -23,26 +25,10 @@ export function LoginPage() {
 	return (
 		<div>
 			<div className="md:hidden"></div>
-			<div className="container relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+			<div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
 				<div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
 					<div className="absolute inset-0 bg-zinc-900" />
-					<Link href="/">
-						<div className="relative z-20 flex items-center text-lg font-medium">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								strokeWidth="2"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								className="mr-2 h-6 w-6"
-							>
-								<path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-							</svg>
-							{texts.EN.companyName}
-						</div>
-					</Link>
+					<Logo />
 					<div className="relative z-20 mt-auto">
 						<blockquote className="space-y-2">
 							<p className="text-lg">{texts.EN.title}</p>
@@ -60,7 +46,7 @@ export function LoginPage() {
 								{texts.EN.enterEmail}
 							</p>
 						</div>
-						<LoginForm error={null} isPending={false} mutate={() => {}} />
+						<LoginForm error={null} isPending={false} mutate={() => { }} />
 						<p className="px-8 text-center text-sm text-muted-foreground">
 							{texts.EN.dontHaveAccount}
 						</p>
--- packages/ui/src/domains/Auth/Register/RegisterPage.tsx
+++ packages/ui/src/domains/Auth/Register/RegisterPage.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { RegisterFormWithHook } from "./RegisterFormWithHook";
 import { RegisterForm } from "@repo/ui/components/form-builder/Forms/Register/RegisterForm";
 import Link from "next/link";
+import { Logo } from "@repo/ui/components/ui/Logo";
 // import { RegisterForm } fr	om "../../../../components/form-builder/Forms/Register";
 
 export const metadata: Metadata = {
@@ -24,27 +25,11 @@ const texts = {
 export function RegisterPage() {
 	return (
 		<>
-			<div className="md:hidden"></div>
-			<div className="container relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+			{/* <div className="md:hidden"></div> */}
+			<div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
 				<div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
 					<div className="absolute inset-0 bg-zinc-900" />
-					<Link href="/">
-						<div className="relative z-20 flex items-center text-lg font-medium">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								strokeWidth="2"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								className="mr-2 h-6 w-6"
-							>
-								<path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-							</svg>
-							{texts.EN.companyName}
-						</div>
-					</Link>
+					<Logo />
 					<div className="relative z-20 mt-auto">
 						<blockquote className="space-y-2">
 							<p className="text-lg">{texts.EN.title}</p>
@@ -62,7 +47,7 @@ export function RegisterPage() {
 								{texts.EN.enterEmail}
 							</p>
 						</div>
-						<RegisterForm error={null} isPending={false} mutate={() => {}} />
+						<RegisterForm error={null} isPending={false} mutate={() => { }} />
 						<p className="px-8 text-center text-sm text-muted-foreground">
 							{texts.EN.terms}
 						</p>
