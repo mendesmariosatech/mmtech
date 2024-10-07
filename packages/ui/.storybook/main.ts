3f20dfd2b68088acc0bc6ad1f5c193752e954d7e
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-10-06 22:35:54 -0400

    format

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -7,26 +7,26 @@ import { join, dirname } from "path";
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
+	return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-interactions"),
-  ],
-  env: (config) => ({
-    ...config,
-    EXAMPLE_VAR: "An environment variable configured in Storybook",
-    LANG: process?.env?.LANG === "EN" ? "EN" : "BR",
-  }),
-  framework: {
-    name: getAbsolutePath("@storybook/nextjs"),
-    options: {},
-  },
-  staticDirs: ["../public"],
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [
+		getAbsolutePath("@storybook/addon-onboarding"),
+		getAbsolutePath("@storybook/addon-links"),
+		getAbsolutePath("@storybook/addon-essentials"),
+		getAbsolutePath("@chromatic-com/storybook"),
+		getAbsolutePath("@storybook/addon-interactions"),
+	],
+	env: (config) => ({
+		...config,
+		EXAMPLE_VAR: "An environment variable configured in Storybook",
+		LANG: process?.env?.LANG === "EN" ? "EN" : "BR",
+	}),
+	framework: {
+		name: getAbsolutePath("@storybook/nextjs"),
+		options: {},
+	},
+	staticDirs: ["../public"],
 };
 export default config;
