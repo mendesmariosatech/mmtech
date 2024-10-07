ae91ac7a7efd71bf1bb11107d26fa3888412c9d6
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-10-06 22:35:46 -0400

    re-install storybook

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,32 +1,32 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
 import { join, dirname } from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value) {
-	return dirname(require.resolve(join(value, "package.json")));
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
 }
-
-/** @type { import('@storybook/nextjs').StorybookConfig } */
-const config = {
-	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-	staticDirs: ["../public"],
-	env: (config) => ({
-		...config,
-		EXAMPLE_VAR: "An environment variable configured in Storybook",
-		LANG: process?.env?.LANG === "EN" ? "EN" : "BR",
-	}),
-	addons: [
-		getAbsolutePath("@storybook/addon-onboarding"),
-		getAbsolutePath("@storybook/addon-links"),
-		getAbsolutePath("@storybook/addon-essentials"),
-		getAbsolutePath("@chromatic-com/storybook"),
-		getAbsolutePath("@storybook/addon-interactions"),
-	],
-	framework: {
-		name: getAbsolutePath("@storybook/nextjs"),
-		options: {},
-	},
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-interactions"),
+  ],
+  env: (config) => ({
+    ...config,
+    EXAMPLE_VAR: "An environment variable configured in Storybook",
+    LANG: process?.env?.LANG === "EN" ? "EN" : "BR",
+  }),
+  framework: {
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {},
+  },
+  staticDirs: ["../public"],
 };
 export default config;
