f07de5bb870935a19acd2ddae1e854339dad2265
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-10-11 02:27:47 -0400

    format

--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -1,5 +1,3 @@
-
-
 module.exports = {
-  testEnvironment: 'node'
+	testEnvironment: "node",
 };
--- packages/server/src/routes/auth/auth.test.ts
+++ packages/server/src/routes/auth/auth.test.ts
@@ -1,11 +1,11 @@
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, test } from "@jest/globals";
 
 const sum = (a: number, b: number) => {
-  return a + b;
-}
+	return a + b;
+};
 
-describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
+describe("sum module", () => {
+	test("adds 1 + 2 to equal 3", () => {
+		expect(sum(1, 2)).toBe(3);
+	});
 });
