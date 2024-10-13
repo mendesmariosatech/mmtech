a295ac7f5d38a19de2927217cc0b6838e978f119
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-10-13 17:16:34 -0400

    format

--- packages/ui/src/components/composition/Lists/EmptyList.tsx
+++ packages/ui/src/components/composition/Lists/EmptyList.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Button } from '../../ui/button'
+import React from "react";
+import { Button } from "../../ui/button";
 
 export const EmptyList = () => {
-  return (
-    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 max-h-full">
-      <div className="flex items-center">
-        <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
-      </div>
-      <div
-        className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm p-6"
-        x-chunk="dashboard-02-chunk-1"
-      >
-        <div className="flex flex-col items-center gap-1 text-center">
-          <h3 className="text-2xl font-bold tracking-tight">
-            You have no products
-          </h3>
-          <p className="text-sm text-muted-foreground">
-            You can start selling as soon as you add a product.
-          </p>
-          <Button className="mt-4">Add Product</Button>
-        </div>
-      </div>
-    </main>
-  )
-}
+	return (
+		<main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 max-h-full">
+			<div className="flex items-center">
+				<h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
+			</div>
+			<div
+				className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm p-6"
+				x-chunk="dashboard-02-chunk-1"
+			>
+				<div className="flex flex-col items-center gap-1 text-center">
+					<h3 className="text-2xl font-bold tracking-tight">
+						You have no products
+					</h3>
+					<p className="text-sm text-muted-foreground">
+						You can start selling as soon as you add a product.
+					</p>
+					<Button className="mt-4">Add Product</Button>
+				</div>
+			</div>
+		</main>
+	);
+};
