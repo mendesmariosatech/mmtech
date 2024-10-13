90c8bbc64d73f6e1787b333e1c6bfa436525a1e0
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-10-12 23:27:57 -0400

    format

--- packages/ui/src/components/ui/composition/DashBoardV2/Dashboard.stories.tsx
+++ packages/ui/src/components/ui/composition/DashBoardV2/Dashboard.stories.tsx
@@ -1,16 +1,14 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Dashboard } from './Dashboard';
+import type { Meta, StoryObj } from "@storybook/react";
+import { Dashboard } from "./Dashboard";
 
 const meta = {
-  title: 'Category',
-  component: Dashboard,
+	title: "Category",
+	component: Dashboard,
 } satisfies Meta<typeof Dashboard>;
-
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const BaseDashboard: Story = {
-  args: {
-  },
+	args: {},
 };
