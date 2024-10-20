b4230601a444f9b247f24b71564f4154d884ee6e
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-10-19 23:11:50 -0400

    Update useLogin.ts

--- packages/hook-services/react-query/useLogin.ts
+++ packages/hook-services/react-query/useLogin.ts
@@ -1,9 +1,13 @@
+"use client";
 import type { LoginFields } from "@repo/zod-types";
 import { useMutation } from "@tanstack/react-query";
 import { hono_client } from "../hono_client";
 import { toast } from "sonner";
+import { Routes } from "@repo/data-testing/Routes";
+import { useRouter } from "next/navigation";
 
 export function useLogin() {
+	const router = useRouter();
 	return useMutation({
 		onSettled: () => {},
 		onError: (error) => {
@@ -21,6 +25,7 @@ export function useLogin() {
 			}
 			console.log("Success Message:", resp.data.name);
 			toast.success("Login successful");
+			router.push(Routes.client["/client/dashboard"]);
 		},
 
 		mutationFn: (data: LoginFields) => {
