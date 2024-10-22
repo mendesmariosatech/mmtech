4b4407d71af53845f633b6f96e4fa5838e458415
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-10-21 21:50:51 -0400

    format

--- packages/backend/app/api/[...route]/route.ts
+++ packages/backend/app/api/[...route]/route.ts
@@ -1,73 +1,78 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-import { OpenAPIHono, z, RouteHandler } from '@hono/zod-openapi'
-import { createRoute } from '@hono/zod-openapi'
-import { apiReference } from '@scalar/hono-api-reference'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { OpenAPIHono, z, RouteHandler } from "@hono/zod-openapi";
+import { createRoute } from "@hono/zod-openapi";
+import { apiReference } from "@scalar/hono-api-reference";
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
-console.log('Hello from Hono!')
-console.log(process.env.ronaldo)
+console.log("Hello from Hono!");
+console.log(process.env.ronaldo);
 
 const route = createRoute({
-  method: 'get',
-  path: "/hello",
-  tags: ['hello'],
-  responses: {
-    404: {
-      description: 'Not found',
-      content: {
-        'application/json': {
-          schema: z.object({
-            NotFound: z.string(),
-          })
-        },
-      },
-    },
-    200: {
-      description: 'Hello from Hono!',
-      content: {
-        'application/json': {
-          schema: z.object({
-            message: z.string(),
-          })
-        },
-      },
-    }
-  },
-})
+	method: "get",
+	path: "/hello",
+	tags: ["hello"],
+	responses: {
+		404: {
+			description: "Not found",
+			content: {
+				"application/json": {
+					schema: z.object({
+						NotFound: z.string(),
+					}),
+				},
+			},
+		},
+		200: {
+			description: "Hello from Hono!",
+			content: {
+				"application/json": {
+					schema: z.object({
+						message: z.string(),
+					}),
+				},
+			},
+		},
+	},
+});
 
-type LoginRoute = typeof route
+type LoginRoute = typeof route;
 
-
-const app = new OpenAPIHono().basePath('/api')
+const app = new OpenAPIHono().basePath("/api");
 
 const routesHello: RouteHandler<LoginRoute> = (c) => {
-  return c.json({
-    message: 'Hello from Hono!'
-  }, 200)
-}
+	return c.json(
+		{
+			message: "Hello from Hono!",
+		},
+		200,
+	);
+};
 
-app.openapi(route, routesHello)
+app.openapi(route, routesHello);
 
 app.doc("/docs", {
-  openapi: "3.0.0",
-  info: {
-    title: "Hono API",
-    version: "1.0.0",
-  },
-})
+	openapi: "3.0.0",
+	info: {
+		title: "Hono API",
+		version: "1.0.0",
+	},
+});
 
-app.get("/scalar", apiReference({
-  theme: 'bluePlanet',
-  layout: "classic",
-  defaultHttpClient: {
-    targetKey: "node",
-    clientKey: "axios",
-  },
-  spec: {
-    url: "/api/docs"
-  }
-}))
+app.get(
+	"/scalar",
+	apiReference({
+		theme: "bluePlanet",
+		layout: "classic",
+		defaultHttpClient: {
+			targetKey: "node",
+			clientKey: "axios",
+		},
+		spec: {
+			url: "/api/docs",
+		},
+	}),
+);
 
-export const GET = handle(app)
+export const GET = handle(app);
