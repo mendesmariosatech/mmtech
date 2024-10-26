c9a71a25d66f9872aad83a24ba8b1bf6e65a9caa
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-10-26 01:23:24 -0400

    login route

--- packages/server/src/routes/auth/index.ts
+++ packages/server/src/routes/auth/index.ts
@@ -1,3 +1,12 @@
+import { app } from "../../base/base-app";
+import { loginHandler, loginSpec } from "./login";
 import { registerHandler, registerSpec } from "./register";
 
-export const authRoutes = [[registerSpec, registerHandler]] as const;
+export const authRoutes = [
+	[registerSpec, registerHandler],
+	[loginSpec, loginHandler],
+] as const;
+
+export const authRouter = authRoutes.map(([route, handler]) =>
+	app.openapi(route, handler),
+);
---
+++ packages/server/src/routes/auth/login.ts
@@ -0,0 +1,109 @@
+import { createRoute, RouteHandler, z } from "@hono/zod-openapi";
+import { loginFields } from "@repo/zod-types";
+import { ENV_TYPES } from "../../env/zod";
+import { env } from "hono/adapter";
+import { AuthTable } from "./handlers";
+import { checkPassword } from "../../utils/bcrypt";
+import { generateToken } from "../../jwt_token";
+import { setCookie } from "hono/cookie";
+import { COOKIES } from "../../env/cookies";
+
+export const loginSpec = createRoute({
+	method: "post",
+	path: "/login",
+	tags: ["auth"],
+	responses: {
+		201: {
+			description: "Register Successful",
+			content: {
+				"application/json": {
+					schema: z.object({
+						data: z.object({
+							name: z.string(),
+							email: z.string(),
+							phone: z.string().nullable(),
+							id: z.string(),
+							emailConfirmedAt: z.string().nullable(),
+							deletedAt: z.string().nullable(),
+							createdAt: z.string(),
+							updateAt: z.string().nullable(),
+						}),
+					}),
+				},
+			},
+		},
+		400: {
+			description: "Bad Request",
+			content: {
+				"application/json": {
+					schema: z.object({
+						error: z.string(),
+					}),
+				},
+			},
+		},
+		403: {
+			description: "Bad Request",
+			content: {
+				"application/json": {
+					schema: z.object({
+						error: z.string(),
+					}),
+				},
+			},
+		},
+		404: {
+			description: "Not Found",
+			content: {
+				"application/json": {
+					schema: z.object({
+						error: z.string(),
+					}),
+				},
+			},
+		},
+	},
+	request: {
+		body: {
+			content: {
+				"application/json": {
+					schema: loginFields,
+				},
+			},
+		},
+	},
+});
+
+type LoginRoute = typeof loginSpec;
+
+export const loginHandler: RouteHandler<LoginRoute> = async (c) => {
+	const form = c.req.valid("json");
+	const {
+		TURSO_AUTH_TOKEN,
+		TURSO_CONNECTION_URL,
+		COOkIE_SECRET_KEY,
+		JWT_SECRET_KEY,
+	} = env<ENV_TYPES>(c);
+
+	if (!form) return c.json({ error: "Invalid form data" }, 400);
+
+	const { email, password } = form;
+
+	const Auth = new AuthTable(TURSO_CONNECTION_URL, TURSO_AUTH_TOKEN);
+	const user = await Auth.findAuthUser(email);
+	if (!user) return c.json({ error: "User not found" }, 404);
+
+	const doesPasswordMatch = await checkPassword(password, user.passwordDigest);
+
+	if (!doesPasswordMatch)
+		return c.json({ error: "Password does not match" }, 403);
+
+	const token = await generateToken(user, JWT_SECRET_KEY);
+
+	setCookie(c, COOKIES.USER_ID, user.email);
+	setCookie(c, COOKIES.USER_TOKEN, token);
+
+	const { passwordDigest: NOT_USE, ...rest } = user;
+
+	return c.json({ data: rest }, 201);
+};
