e96bdc3a0bb7238b948d1f8592ccbbbaa22fe067
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-11-01 23:01:29 -0400

    rename tables and drop old tables
    - Start an events route
    - create a generate id function

---
+++ packages/server/migrations/0000_stiff_prism.sql
@@ -0,0 +1,90 @@
+CREATE TABLE `address` (
+	`id` text(128) PRIMARY KEY NOT NULL,
+	`street_address` text NOT NULL,
+	`complement` text,
+	`state_or_province` text NOT NULL,
+	`city` text NOT NULL,
+	`postal_code` text NOT NULL,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`country` text NOT NULL,
+	`updated_at` integer
+);
+--> statement-breakpoint
+CREATE TABLE `auth` (
+	`id` text(128) PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`password_digest` text NOT NULL,
+	`email` text NOT NULL,
+	`phone` text,
+	`email_confirmed_at` integer,
+	`deleted_at` integer,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` integer
+);
+--> statement-breakpoint
+CREATE TABLE `business_customers` (
+	`id` text(128) PRIMARY KEY NOT NULL,
+	`business_id` text NOT NULL,
+	`customer_id` text NOT NULL,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` integer,
+	FOREIGN KEY (`business_id`) REFERENCES `business`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`customer_id`) REFERENCES `customer`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `business` (
+	`id` text(128) PRIMARY KEY NOT NULL,
+	`client_id` text NOT NULL,
+	`name` text NOT NULL,
+	`address` text,
+	`description` text,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` integer,
+	FOREIGN KEY (`client_id`) REFERENCES `client`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`address`) REFERENCES `address`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `client` (
+	`id` text(128) PRIMARY KEY NOT NULL,
+	`auth_id` text NOT NULL,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` integer,
+	FOREIGN KEY (`auth_id`) REFERENCES `auth`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `customer` (
+	`id` text(128) PRIMARY KEY NOT NULL,
+	`auth_id` text,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` integer,
+	FOREIGN KEY (`auth_id`) REFERENCES `auth`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `customer_attendee` (
+	`id` text(128) PRIMARY KEY NOT NULL,
+	`customer_id` text NOT NULL,
+	`event_id` text NOT NULL,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` integer,
+	FOREIGN KEY (`customer_id`) REFERENCES `customer`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`event_id`) REFERENCES `event`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `event` (
+	`id` text(128) PRIMARY KEY NOT NULL,
+	`client_id` text NOT NULL,
+	`business_id` text NOT NULL,
+	`address` text,
+	`title` text NOT NULL,
+	`description` text,
+	`date` integer NOT NULL,
+	`time` integer,
+	`location` text,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` integer,
+	FOREIGN KEY (`client_id`) REFERENCES `client`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`business_id`) REFERENCES `business`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`address`) REFERENCES `address`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `auth_email_unique` ON `auth` (`email`);
--- packages/server/src/drizzle/schema.ts
+++ packages/server/src/drizzle/schema.ts
@@ -1,10 +1,15 @@
 import { createId } from "@paralleldrive/cuid2";
-import { relations, sql } from "drizzle-orm";
+import { name, relations, sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { z } from "zod";
+
+const Initial = z.string().min(2).max(2).toUpperCase();
+const genEntityId = (initials: string) =>
+	`${Initial.parse(initials)}_${createId().toUpperCase()}`;
 
 export const authTable = sqliteTable("auth", {
 	id: text("id", { length: 128 })
-		.$defaultFn(() => `AU_` + createId())
+		.$defaultFn(() => genEntityId("AU") + createId())
 		.primaryKey(),
 	name: text("name").notNull(),
 	passwordDigest: text("password_digest").notNull(),
@@ -23,7 +28,7 @@ export type SelectAuth = typeof authTable.$inferSelect;
 
 export const clientTable = sqliteTable("client", {
 	id: text("id", { length: 128 })
-		.$defaultFn(() => `CL_` + createId())
+		.$defaultFn(() => genEntityId(`CL`))
 		.primaryKey(),
 	authId: text("auth_id")
 		.references(() => authTable.id, { onDelete: "cascade" })
@@ -43,3 +48,163 @@ export const clientRelations = relations(clientTable, ({ one }) => ({
 
 export type InsertClient = typeof clientTable.$inferInsert;
 export type SelectClient = typeof clientTable.$inferSelect;
+
+export const customer = sqliteTable("customer", {
+	id: text("id", { length: 128 })
+		.$defaultFn(() => genEntityId(`CU`))
+		.primaryKey(),
+	authId: text("auth_id").references(() => authTable.id, {
+		onDelete: "cascade",
+	}),
+	createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`).notNull(),
+	updateAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(
+		() => new Date(),
+	),
+});
+
+export const customerRelations = relations(customer, ({ one }) => ({
+	auth: one(authTable, {
+		fields: [customer.authId],
+		references: [authTable.id],
+	}),
+}));
+
+export const businessTable = sqliteTable("business", {
+	id: text("id", { length: 128 })
+		.$defaultFn(() => genEntityId("BS"))
+		.primaryKey(),
+	clientId: text("client_id")
+		.references(() => clientTable.id, { onDelete: "cascade" })
+		.notNull(),
+	name: text("name").notNull(),
+	address: text("address").references(() => addressTable.id, {
+		onDelete: "cascade",
+	}),
+	description: text("description"),
+	createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`).notNull(),
+	updateAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(
+		() => new Date(),
+	),
+});
+
+export const businessRelations = relations(businessTable, ({ one }) => ({
+	client: one(clientTable, {
+		fields: [businessTable.clientId],
+		references: [clientTable.id],
+	}),
+}));
+
+export const eventTable = sqliteTable("event", {
+	id: text("id", { length: 128 })
+		.$defaultFn(() => genEntityId(`EV`))
+		.primaryKey(),
+	client_creator: text("client_id")
+		.references(() => clientTable.id, { onDelete: "cascade" })
+		.notNull(),
+	business_id: text("business_id")
+		.references(() => businessTable.id, { onDelete: "cascade" })
+		.notNull(),
+	address: text("address").references(() => addressTable.id, {
+		onDelete: "cascade",
+	}),
+	title: text("title").notNull(),
+	description: text("description"),
+	date: integer("date").notNull(),
+	time: integer("time"),
+	location: text("location"),
+	createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`).notNull(),
+	updateAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(
+		() => new Date(),
+	),
+});
+
+export const eventRelations = relations(eventTable, ({ one }) => ({
+	client: one(clientTable, {
+		fields: [eventTable.client_creator],
+		references: [clientTable.id],
+	}),
+	business: one(businessTable, {
+		fields: [eventTable.business_id],
+		references: [businessTable.id],
+	}),
+	addres: one(addressTable, {
+		fields: [eventTable.address],
+		references: [addressTable.id],
+	}),
+}));
+
+export const customerAttendeeTable = sqliteTable("customer_attendee", {
+	id: text("id", { length: 128 })
+		.$defaultFn(() => genEntityId(`CA`))
+		.primaryKey(),
+	customerId: text("customer_id")
+		.references(() => customer.id, { onDelete: "cascade" })
+		.notNull(),
+	eventId: text("event_id")
+		.references(() => eventTable.id, { onDelete: "cascade" })
+		.notNull(),
+	createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`).notNull(),
+	updateAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(
+		() => new Date(),
+	),
+});
+
+export const customerAttendeeRelations = relations(
+	customerAttendeeTable,
+	({ one }) => ({
+		customer: one(customer, {
+			fields: [customerAttendeeTable.customerId],
+			references: [customer.id],
+		}),
+		event: one(eventTable, {
+			fields: [customerAttendeeTable.eventId],
+			references: [eventTable.id],
+		}),
+	}),
+);
+
+export const businessCustomers = sqliteTable("business_customers", {
+	id: text("id", { length: 128 })
+		.$defaultFn(() => genEntityId(`BC`))
+		.primaryKey(),
+	businessId: text("business_id")
+		.references(() => businessTable.id, { onDelete: "cascade" })
+		.notNull(),
+	customerId: text("customer_id")
+		.references(() => customer.id, { onDelete: "cascade" })
+		.notNull(),
+	createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`).notNull(),
+	updateAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(
+		() => new Date(),
+	),
+});
+
+export const businessCustomersRelations = relations(
+	businessCustomers,
+	({ one }) => ({
+		business: one(businessTable, {
+			fields: [businessCustomers.businessId],
+			references: [businessTable.id],
+		}),
+		customer: one(customer, {
+			fields: [businessCustomers.customerId],
+			references: [customer.id],
+		}),
+	}),
+);
+
+export const addressTable = sqliteTable("address", {
+	id: text("id", { length: 128 })
+		.$defaultFn(() => genEntityId(`AD`))
+		.primaryKey(),
+	streetAddress: text("street_address").notNull(),
+	complement: text("complement"),
+	stateOrProvince: text("state_or_province").notNull(),
+	city: text("city").notNull(),
+	postalCode: text("postal_code").notNull(),
+	createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`).notNull(),
+	country: text("country").notNull(),
+	updateAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(
+		() => new Date(),
+	),
+});
