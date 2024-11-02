f593256762841cb007d3b7a5356ad0705c8ed80f
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-11-02 15:41:21 -0400

    revert mock

--- packages/server/src/routes/auth/index.test.ts
+++ packages/server/src/routes/auth/index.test.ts
@@ -11,17 +11,17 @@ const testPassword = "12312349090ASAKkdk";
 
 const SECONDS = 1000;
 jest.setTimeout(70 * SECONDS);
-// jest.mock("../../jwt_token", () => {
-// 	return {
-// 		generateToken: jest.fn().mockReturnValue(Promise.resolve("123")),
-// 		decodeToken: jest.fn().mockReturnValue(
-// 			Promise.resolve({
-// 				jwtPayload: "123",
-// 				email: "email@gmail.com",
-// 			}),
-// 		),
-// 	};
-// });
+jest.mock("../../jwt_token", () => {
+	return {
+		generateToken: jest.fn().mockReturnValue(Promise.resolve("123")),
+		decodeToken: jest.fn().mockReturnValue(
+			Promise.resolve({
+				jwtPayload: "123",
+				email: "email@gmail.com",
+			}),
+		),
+	};
+});
 
 describe("New User - POST /auth/register", () => {
 	afterAll(async () => {
