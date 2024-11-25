1199c85397e63998e4883d58f68ce6b702034137
Michele Stefanoni <153405495+MiStefanoni@users.noreply.github.com>
2024-11-25 14:31:55 -0500

    Update button.stories.ts

--- packages/ui/src/components/ui/button.stories.ts
+++ packages/ui/src/components/ui/button.stories.ts
@@ -2,3 +2,12 @@ import { Meta, StoryObj } from "@storybook/react";
 import { Button } from "./button";
 
 // ronaldo
+const meta = {
+	title: "Button",
+	component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const ButtonPrimary: Story = {};
