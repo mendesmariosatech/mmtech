01bb9d236d84f375b608ecb64cbdeaa1fe65b277
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-11-26 20:35:08 -0500

    test husky changes

--- packages/server/src/routes/middleware/dbContext.ts
+++ packages/server/src/routes/middleware/dbContext.ts
@@ -5,8 +5,14 @@ import { AppOpenAPI } from "../../base/type";
 
 export const dbContext = createMiddleware<any, string, {}, AppOpenAPI>(
 	async (c, next) => {
-		const { TURSO_AUTH_TOKEN, TURSO_CONNECTION_URL } = env(c);
+		const {
+			TURSO_AUTH_TOKEN,
 
+			TURSO_CONNECTION_URL,
+		} = env(c);
+
+		// the function here will receive the env variables as an object
+		// and will return all of the DTOs that are needed for the app
 		const db = new DBConnection(TURSO_CONNECTION_URL, TURSO_AUTH_TOKEN);
 
 		c.set("db", db);
