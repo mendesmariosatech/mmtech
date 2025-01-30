53b96e9e9545daddbbb4016f24b04f9bcd991ba4
alex <mendes.develop@gmail.com>
2025-01-29 23:43:01 -0300

    add post endpoint

---
+++ packages/server/src/drizzle/landing-page/component/component.dto.ts
@@ -0,0 +1,63 @@
+import { eq } from "drizzle-orm";
+import { DBConnection } from "../../drizzle-client";
+import { componentPage, SelectComponentPageSchema } from "../page/page";
+import {
+	componentTable,
+	ContentSchema,
+	CssSchema,
+	type CreateComponentSchema,
+} from "./component";
+
+export class ComponentTable extends DBConnection {
+	constructor(TURSO_CONNECTION_URL: string, TURSO_AUTH_TOKEN: string) {
+		super(TURSO_CONNECTION_URL, TURSO_AUTH_TOKEN);
+	}
+
+	public async createComponent(args: CreateComponentSchema) {
+		const cssArgs = CssSchema.parse(args.css);
+		const content = ContentSchema.parse(args.content);
+
+		const [page] = await this.db
+			.insert(componentTable)
+			.values({
+				type: args.type,
+				css: cssArgs,
+				content: content,
+			})
+			.returning();
+
+		return page;
+	}
+
+	public async getAllComponentsByTemplate(
+		pageId: SelectComponentPageSchema["id"],
+	) {
+		const components = await this.db.query.componentPage.findMany({
+			where: eq(componentTable.id, pageId),
+		});
+
+		return components;
+	}
+
+	// update component
+	public async updateComponent(
+		args: CreateComponentSchema & {
+			id: number;
+		},
+	) {
+		const cssArgs = CssSchema.parse(args.css);
+		const content = ContentSchema.parse(args.content);
+
+		const [page] = await this.db
+			.update(componentTable)
+			.set({
+				type: args.type,
+				css: cssArgs,
+				content: content,
+			})
+			.where(eq(componentTable.id, args.id))
+			.returning();
+
+		return page;
+	}
+}
